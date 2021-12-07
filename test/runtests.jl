c89e0651d0f87bd9bd8dcbe6ed7ea0872669d9f9
Xenia Ibach <93128302+xibach@users.noreply.github.com>
2021-12-07 20:45:59 +0100

    New test for calculating the albedo

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,6 +15,16 @@ EXAMPLES_DIR = joinpath(pathof(Klimakoffer) |> dirname |> dirname, "examples")
     @test isapprox(GlobTemp, 14.484963368768746, atol=1e-12)
   end
 
+  test_file = "equilibrium_temperature_albedo.jl" 
+  @testset "$test_file" begin
+    println("")
+    println("Running ",test_file)
+    println("")
+    @test_nowarn include(joinpath(EXAMPLES_DIR, test_file))
+
+    @test isapprox(GlobTemp, 15.007699256043672, atol=1e-12)
+  end
+
   test_file = "equilibrium_temperature_co2.jl" 
   @testset "$test_file" begin
     println("")
@@ -25,14 +35,14 @@ EXAMPLES_DIR = joinpath(pathof(Klimakoffer) |> dirname |> dirname, "examples")
     @test isapprox(GlobTemp, 14.484963368768978, atol=1e-12)
   end
 
-  test_file = "transient_temperature_co2.txt"
+  test_file = "equilibrium_temperature_co2_transient.jl"
   @testset "$test_file" begin
     println("")
     println("Running ",test_file)
     println("")
     @test_nowarn include(joinpath(EXAMPLES_DIR, test_file))
 
-    @test isapprox(sol.mean_temperature_yearly[end], 15.124711386303053, atol=1e-12)
+    @test isapprox(sol.mean_temperature_yearly[end], 15.124711376823102, atol=1e-12)
   end
 
   @testset "Printing types to the REPL" begin
